b16016abdd1cc6bafdca26d7a36e9d7b378fbe94
alearm246 <47065061+alearm246@users.noreply.github.com>
2022-08-08 12:17:16 -0500

    add basic file structure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Wordle Game!!!</h1>
+        <h1>Wordle Game in React JS!!!</h1>
       </header>
     </div>
   );
