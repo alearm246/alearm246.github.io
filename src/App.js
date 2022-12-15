c6d1396b25a2d3e2f418981206ab6ad54810a83e
alearm246 <47065061+alearm246@users.noreply.github.com>
2022-12-15 16:54:41 -0600

    implement basic wordle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import Board from "../src/components/common/Board/Board";
+import KeyBoard from "../src/components/common/KeyBoard/KeyBoard";
+import { BoardContextProvider } from './context/BoardContext';
+import useSecretWord from './customHooks/useSecretWord';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
         <h1>Wordle Game in React JS!!!</h1>
       </header>
+      <BoardContextProvider>
+        <div className="game-container">
+          <div className="board-container">
+            <Board />
+          </div>
+          <div className="keyboard-container">
+            <KeyBoard />
+          </div>  
+        </div> 
+      </BoardContextProvider>
     </div>
   );
 }
