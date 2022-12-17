a6df971262b189fa32b26b2687e7739c5008028d
alearm246 <47065061+alearm246@users.noreply.github.com>
2022-12-16 22:56:32 -0600

    Updates

--- src/utils/isValidWord.js
+++
@@ -1,8 +0,0 @@
-import validWords from "../data/words";
-
-function isValidWord(guessedWord) {
-    if(!validWords[guessedWord]) return false;
-    return validWords[guessedWord];
-}
-
-export default isValidWord;
